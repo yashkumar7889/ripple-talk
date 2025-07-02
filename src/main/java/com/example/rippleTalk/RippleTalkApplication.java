f894a9842d7c107e1a6279efd4a0815857197943
Yash Kumar Singh Patwa <yashkumarsinghpatwa7889@gmail.com>
2025-07-02 22:07:32 +0530

    Initial Commit

---
+++ src/main/java/com/example/rippleTalk/RippleTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.rippleTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RippleTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RippleTalkApplication.class, args);
+	}
+
+}
