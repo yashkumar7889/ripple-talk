eb6c6e61995c4745d1b262dbb9caecf59b8561cc
Yash Kumar Singh Patwa <yashkumarsinghpatwa7889@gmail.com>
2025-08-26 22:47:40 +0530

    Added check for users when conversation request is sent

--- src/main/java/com/example/rippleTalk/controller/ConversationRequestController.java
+++ src/main/java/com/example/rippleTalk/controller/ConversationRequestController.java
@@ -3,6 +3,9 @@ package com.example.rippleTalk.controller;
 import com.example.rippleTalk.dto.AcceptConversationRequest;
 import com.example.rippleTalk.dto.ConversationRequestDto;
 import com.example.rippleTalk.dto.ConversationRequestResponseDto;
+import com.example.rippleTalk.entity.User;
+import com.example.rippleTalk.exception.ResourceNotFoundException;
+import com.example.rippleTalk.repository.UserRepository;
 import com.example.rippleTalk.service.ConversationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +15,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @RequestMapping("/api/conversation")
 @RestController
 @RequiredArgsConstructor
@@ -19,9 +27,18 @@ public class ConversationRequestController
 {
     private final ConversationService conversationService;
 
+    private final UserRepository userRepository;
+
     @PostMapping("/request")
     public ResponseEntity<ConversationRequestResponseDto> sendRequest(
            @Valid @RequestBody ConversationRequestDto dto) {
+        List<String> missingUserIds = getMissingUsers(List.of(dto.getReceiverId(), dto.getSenderId()));
+        if(!missingUserIds.isEmpty())
+        {
+            String errorMessage = "One or more users not found: " + String.join(", ", missingUserIds);
+            throw new ResourceNotFoundException(errorMessage);
+        }
+
         return ResponseEntity.ok(conversationService.sendRequest(dto));
     }
 
@@ -30,5 +47,13 @@ public class ConversationRequestController
            @Valid @RequestBody AcceptConversationRequest dto) {
 
         return ResponseEntity.ok(conversationService.respondToRequest(dto));
+    }
+
+    private List<String> getMissingUsers(List<String> userIds)
+    {
+        Optional<User> users = userRepository.findByUsernameInOrEmailInIgnoreCase(userIds);
+        Set<String> foundUserIds = users.stream().map(User::getId).collect(Collectors.toSet());
+        return userIds.stream()
+                .filter(id -> !foundUserIds.contains(id)).toList();
     }
 }
